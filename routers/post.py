62e083c9336fd012e4e103f188bb34b52a3aae1b
Diana Dvoryak <dianadvoryak.w@gmail.com>
2023-11-06 14:44:35 +0200

    upload image

--- routers/post.py
+++ routers/post.py
@@ -29,3 +29,17 @@ def create(request: PostBase, db: Session = Depends(get_db)):
 @router.get('/all', response_model=List[PostDisplay])
 def posts(db: Session = Depends(get_db)):
   return db_post.get_all(db)
+
+
+@router.post('/image')
+def upload_image(image: UploadFile = File(...)):
+    letters = string.ascii_letters
+    rand_str = ''.join(random.choice(letters) for i in range(6))
+    new = f'_{rand_str}.'
+    filename = new.join(image.filename.rsplit('.', 1))
+    path = f'images/{filename}'
+
+    with open(path, "w+b") as buffer:
+        shutil.copyfileobj(image.file, buffer)
+
+    return {'filename': path}
